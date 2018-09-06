3edf8dd45e6f551b079afbd783deeb1aba357ba9
chenzhe512 <chenzhe512@gmail.com>
2018-09-07 01:23:22 +0800

    修改index.html

--- mian.js
+++ mian.js
@@ -1,19 +1,41 @@
 !function(){
+    var duration = 20
+
+    $('.actions').on('click','button',function(e){
+        let $button = $(e.currentTarget)
+        let speed = $button.attr('data-speed')
+        $button.addClass('active')
+        .siblings('.active').removeClass('active')
+        switch(speed){
+            case 'slow':
+            duration = 100
+            break
+            case 'normal':
+            duration = 50
+            break
+            case 'fast':
+            duration = 10
+            break
+        }
+    })
+    
     function writeCode(prefix,code,fn){
         let container = document.querySelector('#code')
         let styleTag = document.querySelector('#styleTag')
         let n = 0
-        let id = setInterval(function(){
+        setTimeout(function run(){
             n += 1
             container.innerHTML = code.slice(0,n)
             styleTag.innerHTML = code.slice(0,n)
             container.scrollTop = container.scrollHeight
-            if(n >= code.length){
-                window.clearInterval(id)
+            if(n < code.length){
+                setTimeout(run,duration)
+            }else{
                 fn && fn.call()
+
             }
             
-        },0)
+        },duration)
     }
     let code = `
     /*
@@ -164,4 +186,6 @@
       */
     `
     writeCode('',code)
+
+
 }.call()
